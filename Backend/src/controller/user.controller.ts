11474d7222979f9c27c93c2cb3ec3680768fb0ed
SuSyed786 <148403504+SuSyed786@users.noreply.github.com>
2023-10-28 22:37:28 -0500

    ExtractMethod refactor

--- Backend/src/controller/user.controller.ts
+++ Backend/src/controller/user.controller.ts
@@ -1,46 +1,56 @@
-/**
- * REFACTORING AND STRICT TYPE CHECK REQUIRED
- */
-import {User as UserDetails} from "../types/user.type";
-
-import {NextFunction, Request, Response} from "express";
-import {db} from "../server";
-import {ObjectId, WithId} from "mongodb";
+import { User as UserDetails } from "../types/user.type";
+import { NextFunction, Request, Response } from "express";
+import { db } from "../server";
+import { ObjectId, WithId } from "mongodb";
 import HTTP from "../utils/statusCodeConfig";
+
 function sendJSONResponse(res: Response, statusCode: number, data: any) {
   res.status(statusCode).json(data);
 }
+
 export default class User {
   constructor() {}
-  
+
   async getUsers(req: Request, res: Response, next: NextFunction) {
-    const users = <WithId<UserDetails>[]>(
-      await db.collection("Users").find().toArray()
-    );
-    sendJSONResponse(res, HTTP.OK, users);
+    const users = await this.fetchUsers();
+    this.respondWithJSON(res, HTTP.OK, users);
   }
 
   async getProfile(req: Request, res: Response, next: NextFunction) {
-    const user = <WithId<UserDetails>>await db.collection("Users").findOne(
-      {_id: new ObjectId((req as any).user._id)},
-      {
-        projection: {
-          username: 1,
-          phone_number: 1,
-          email: 1,
-          emergency_contact: 1,
-        },
-      }
-    );
-    sendJSONResponse(res, HTTP.OK, {
+    const user = await this.fetchUserProfile(req);
+    this.respondWithJSON(res, HTTP.OK, {
       ...user,
-      image: process.env.MINIO_IMAGE_URL!.replace("%USERNAME%", user.username),
+      image: this.getUserImageUrl(user.username),
     });
   }
 
   async uploadImage(req: Request, res: Response, next: NextFunction) {
-    sendJSONResponse(res, HTTP.OK, {
+    this.respondWithJSON(res, HTTP.OK, {
       message: "Image uploaded",
     });
+  }
+
+  private async fetchUsers(): Promise<WithId<UserDetails>[]> {
+    return await db.collection("Users").find().toArray();
+  }
+
+  private async fetchUserProfile(req: Request): Promise<WithId<UserDetails>> {
+    const userId = (req as any).user._id;
+    return await db.collection("Users").findOne({ _id: new ObjectId(userId) }, {
+      projection: {
+        username: 1,
+        phone_number: 1,
+        email: 1,
+        emergency_contact: 1,
+      },
+    });
+  }
+
+  private respondWithJSON(res: Response, statusCode: number, data: any) {
+    sendJSONResponse(res, statusCode, data);
+  }
+
+  private getUserImageUrl(username: string): string {
+    return process.env.MINIO_IMAGE_URL!.replace("%USERNAME%", username);
   }
 }
